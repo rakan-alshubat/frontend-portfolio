7c8e5f27d1d30a9454973c8bb47c836d8fde7337
Rakan Alshubat <rakan.alshubat@gmail.com>
2023-10-03 23:57:38 -0700

    testing AWS Amplify connection

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -5,10 +5,10 @@ import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function portfolio() {
   return (
     <>
-      Rakans Main Page
+      HELLO SECOND PAGE
     </>
   )
 }
