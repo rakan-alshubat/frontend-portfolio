47af2ab81d306200a6269ac728be9fbb449ff4bc
Rakan Alshubat <rakan.alshubat@gmail.com>
2025-01-12 22:14:35 -0800

    reverting

--- src/pages/DragRace.js
+++ src/pages/DragRace.js
@@ -1,57 +1,22 @@
-import { use, useEffect } from 'react';
 import BarChart from "@/components/BarChartContainer";
 import WelcomeText from "@/components/WelcomeText";
 import calculatePoints from '@/helpers/calculatePoints'
-import gameData from '../files/gameData'
-import QueenSelection from "@/components/QueenSelection";
-import { Button } from '@mui/material';
 
 export default function DragRace() {
   var points
-  var fileNames
-
-  const pullFiles = async () => {
-    const response = await fetch('/api/pull-files', {
-    method: 'POST',
-    headers: {
-      'Concent-Type': 'application/json'
-    },
-    })
-    const data = await response.json()
-    fileNames = data.message
-    console.log(fileNames)
-  };
-
-  useEffect(() => {
-    pullFiles();
-  }, [])
-
-  var hasItStarted = gameData.winnersList.length > 0
 
     playerData.map((player) => {
       points = calculatePoints(player, actualRankings, actualWinners)
     })
-
     return (
       <>
         <WelcomeText
           title={'Rakan\'s Fantasy Drag Race League'}
         />
-        {hasItStarted  && 
-        <>
-          <BarChart />
-        </>
-        }
-        {!hasItStarted  && 
-        <>
-          <QueenSelection />
-        </>
-        }
-        <Button onClick={() => pullFiles()}>test</Button>
+        <BarChart />
       </>
     )
   }
-
 
 const playerData = [
   {
