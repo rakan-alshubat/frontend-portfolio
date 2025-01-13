0d50f307f1a4462acdbb5543a034d7173f1ced15
Rakan Alshubat <rakan.alshubat@gmail.com>
2025-01-12 23:36:49 -0800

    bug fixing

--- src/pages/api/pull-files.js
+++ src/pages/api/pull-files.js
@@ -1,8 +1,10 @@
 import fs from 'fs';
 
 export default async function handler(req, res) {
+  console.log('yes')
     if (req.method === 'POST') {
       try {  
+        console.log(process.cwd() + '\\src\\files\\players')
         const jsonFiles = fs.readdirSync(process.cwd() + '\\src\\files\\players')
         res.status(200).json({ message: jsonFiles});
       } catch (error) {
